ee4c86f962430fad4586d7e779b95790560869da
sadnixon <askspencerhill@berkeley.edu>
2026-09-09 09:49:30 -0700

    test

--- commands/admingamestate.js
+++ commands/admingamestate.js
@@ -32,11 +32,11 @@ async function execute(interaction, user) {
     content: "Here's the current game state!",
     ephemeral: true,
   });
-  await sendGameState(interaction.client, 'current', true, 'none', interaction);
   const gameState = await gameInfo.get('gameState');
   const timers = await schedDB.get('scheduled_tasks');
   console.log(gameState);
   console.log(timers);
+  await sendGameState(interaction.client, 'current', true, 'none', interaction);
   for (const taskId of timers) {
     const task = await schedDB.get(`scheduled_task:${taskId}`);
 
