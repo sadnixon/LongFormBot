e0c1e4d8e00020c09a265f34e8d7e24d8bca2aa2
sadnixon <askspencerhill@berkeley.edu>
2026-09-21 00:58:37 -0700

    oops

--- commands/out.js
+++ commands/out.js
@@ -35,7 +35,7 @@ async function execute(interaction, user) {
     await gameInfo.set('players', currentPlayers);
 
     await genChannel.send(
-      `<@${interaction.user.id}> has now left the lobby! Player count is at ${currentPlayers.length}/17.`,
+      `<@${interaction.user.id}> has now left the lobby! Player count is at ${currentPlayers.length}/Infinity.`,
     );
     await interaction.editReply({
       content: `You have left the lobby!`,
